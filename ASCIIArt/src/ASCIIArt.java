f218df7ed5eccff0cff81655d265d146850536e6
s-shiji <s-shiji@53S500042229.it.bsd405.org>
2019-09-20 15:15:50 -0700

    turn in project1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,24 +1,40 @@
-/**
- * Unit 1 Project
- * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
- * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
- * 
- * @author Mr. Rasmussen 
- *
- */
-
-// YOU CODE HERE :) 
-
 public class ASCIIArt {
-	public static void main (String [] args) {
-		// Add new code
-		System.out.println("ASCII ART");
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		top1();
+		top2();
+		body();
+		body();
+		body();
+		body();
+		body();
+		body();
+		bottom();
+	
+
+	}
+
+	public static void top1() {
+		System.out.println("        |=|");
+		System.out.println("        |=|");
+		System.out.println("        |=|");
+	}
+
+	public static void top2() {
+		System.out.println("         |");
+		System.out.println("       /   \\");
+		System.out.println("     /       \\");
+	    System.out.println("   /           \\");
+	    System.out.println(" /               \\");
+	    System.out.println("|-----------------|");
+	}
+	
+	public static void body() {
+		System.out.println("|                 |");
+	}
+	
+	public static void bottom() {
+		System.out.println("|_________________|");
 	}
 }
